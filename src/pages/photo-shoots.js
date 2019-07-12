9f97c63848526e86c6204beb4c392d147ecb4246
jacobsilver2 <jacobsilver2@mac.com>
2019-07-12 11:10:06 -0400

    build form for photo shoots

--- src/pages/photo-shoots.js
+++ src/pages/photo-shoots.js
@@ -45,20 +45,154 @@ const PhotoShootsPage = ({ data }) => {
     <>
       <Layout fluid={data.file.childImageSharp.fluid}>
         <div>{myhtml}</div>
-          <div className="container">
-            <div className="field">
-              <label className="label is-medium">Medium input</label>
-                <div className="control has-icons-left has-icons-right">
-                  <input className="input is-medium" type="email" placeholder="Extra small"/>
-                  <span className="icon is-small is-left">
-                    <i className="fas fa-envelope fa-xs"></i>
-                  </span>
-                  <span className="icon is-small is-right">
-                    <i className="fas fa-check fa-xs"></i>
-                  </span>
+        <div className="container">
+
+          <div className="field">
+              <label className="label is-small has-text-white">Name</label>
+              <div className="field">
+                <p className="control is-expanded">
+                  <input
+                    className="input"
+                    type="text"
+                    placeholder="First and Last Name"
+                  />
+                </p>
+              </div>
+          </div>
+
+          <div className="field">
+            <label className="label is-small has-text-white">Phone</label>
+              <div className="field is-expanded">
+                <div className="field has-addons">
+                  <p className="control">
+                    <a className="button is-static">+1</a>
+                  </p>
+                  <p className="control is-expanded">
+                    <input
+                      className="input"
+                      type="tel"
+                      placeholder="Phone Number"
+                    />
+                  </p>
                 </div>
+              </div>
+          </div>
+
+          <div className="field">
+            <label className="label is-small has-text-white">Email</label>
+              <div className="field is-expanded">
+                  <p className="control is-expanded">
+                    <input
+                      className="input"
+                      type="email"
+                      placeholder="Email"
+                    />
+                  </p>
+              </div>
+          </div>
+
+          <div className="field">
+              <label className="label is-small has-text-white">Total number of cast and crew</label>
+              <div className="field">
+                <p className="control is-expanded">
+                  <input
+                    className="input"
+                    type="text"
+                  />
+                </p>
+              </div>
+          </div>
+
+          <div className="field">
+              <label className="label is-small has-text-white">Type of shoot (film/video/still) *</label>
+              <div className="field">
+                <p className="control is-expanded">
+                  <textarea
+                    className="textarea"
+                  ></textarea>
+                </p>
+              </div>
+          </div>
+
+          <div className="field">
+              <label className="label is-small has-text-white">Desired date</label>
+              <div className="field">
+                <p className="control is-expanded">
+                  <input
+                    className="input"
+                    type="text"
+                  />
+                </p>
+              </div>
+          </div>
+
+          <div className="field">
+              <label className="label is-small has-text-white">Desired hours (total hours and which hours you desire)</label>
+              <div className="field">
+                <p className="control is-expanded">
+                  <input
+                    className="input"
+                    type="text"
+                  />
+                </p>
+              </div>
+          </div>
+
+          <div className="field">
+              <label className="label is-small has-text-white">Name of production</label>
+              <div className="field">
+                <p className="control is-expanded">
+                  <input
+                    className="input"
+                    type="text"
+                  />
+                </p>
+              </div>
+          </div>
+
+          <div className="field">
+              <label className="label is-small has-text-white">Production company</label>
+              <div className="field">
+                <p className="control is-expanded">
+                  <input
+                    className="input"
+                    type="text"
+                  />
+                </p>
+              </div>
+          </div>
+
+          <div className="field">
+              <label className="label is-small has-text-white">Where would you like to shoot (bar, live room, back yard)</label>
+              <div className="field">
+                <p className="control is-expanded">
+                  <input
+                    className="input"
+                    type="text"
+                  />
+                </p>
+              </div>
+          </div>
+
+          <div className="field">
+              <label className="label is-small has-text-white">How many vehicles associated with shoot</label>
+              <div className="field">
+                <p className="control is-expanded">
+                  <input
+                    className="input"
+                    type="text"
+                  />
+                </p>
+              </div>
+          </div>
+
+          <div className="field">
+            <div className="control">
+              <button className="button is-link has-background-danger">Submit</button>
             </div>
           </div>
+
+        </div>
       </Layout>
     </>
   )
