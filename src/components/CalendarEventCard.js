4b137b83d5dcb017654f15b690e36fde410baab5
jacobsilver2 <jacobsilver2@mac.com>
2019-07-18 08:06:12 -0400

    working on calendar event card

---
+++ src/components/CalendarEventCard.js
@@ -0,0 +1,132 @@
+import React from "react"
+import fakeBlurb from "../utility/fakeblurb"
+import moment from "moment"
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+import Img from 'gatsby-image';
+
+const Image = styled.div`
+  margin: auto;
+  position: relative;
+  box-shadow: 0 15px 25px rgba(0, 0, 0, 0.2);
+  transition: all 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  border-radius: 0.4rem;
+  min-height: 300px;
+  img {
+    border-radius: 0.4rem;
+  }
+  &:hover {
+    box-shadow: 0 40px 45px rgba(0, 0, 0, 0.1);
+    transform: scale(1.04);
+  }
+  a {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    border-radius: 0.4rem;
+    > div {
+      position: static !important;
+    }
+    > div > div {
+      position: static !important;
+    }
+    &:focus {
+      outline: none;
+      box-shadow: 0 0 0 5px #284187;
+    }
+  }
+  flex-basis: 100%;
+  max-width: 100%;
+  width: 100%;
+  @media (max-width: 800px) {
+    flex-basis: 100%;
+    max-width: 100%;
+    width: 100%;
+    margin-bottom: 1.5rem;
+  }
+  @media (max-width: 500px) {
+    min-height: 200px;
+  }
+`;
+
+const Information = styled.div`
+  h1 {
+    font-size: 2rem;
+    display: inline-block;
+    color: white;
+    transition: all 0.4s;
+    &:hover {
+      color: #3498db;
+    }
+  }
+  text-align: center;
+  flex-basis: 100%;
+  max-width: 100%;
+  width: 100%;
+  @media (max-width: 800px) {
+    flex-basis: 100%;
+    max-width: 100%;
+    width: 100%;
+  }
+`;
+
+const Date = styled.div`
+  margin-top: 1rem;
+  color: white;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+`;
+
+const Wrapper = styled.article`
+  margin: 0 3rem;
+`;
+
+function renderImage(image) {
+  return (
+    <Image>
+      <Img fluid={image}/>
+      {/* <img src={image} alt="placeholder" /> */}
+    </Image>
+  )
+}
+
+function renderDate(date) {
+  const formattedDate = moment.utc(date).format("dddd, MMMM DD")
+  return <Date>{formattedDate}</Date>
+}
+
+function renderName(name, date) {
+  const time = moment.utc(date).format("h:mma")
+  return (
+    <Title>
+      {time} - {name}
+    </Title>
+  )
+}
+
+
+function CalendarEventCard(data) {
+  console.log(data)
+  return (
+    <section className="section" key={data.id}>
+      <Wrapper>
+        {data.Act_Image ? renderImage(data.Act_Image.localFiles[0].childImageSharp.fluid) : null}
+        <Information>
+          {renderDate(data.Date)}
+          {data.Act_Website ? (
+            <a href={data.Act_Website[0]}>{renderName(data.Name, data.Date)}</a>
+          ) : (
+            renderName(data.Name, data.Date)
+          )}
+        </Information>
+      </Wrapper>
+
+    </section>
+  )
+}
+
+export default CalendarEventCard;
--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -2,6 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import CalendarEvent from '../components/CalendarEvent';
+import CalendarEventCard from '../components/CalendarEventCard';
 
 export const pageQuery = graphql`
   {
@@ -16,8 +17,12 @@ export const pageQuery = graphql`
           Act_Blurb
           Act_Website
           Act_Image {
-            raw {
-              url
+            localFiles {
+              childImageSharp {
+                fluid(maxWidth: 300) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
             }
           }
         }
@@ -30,7 +35,7 @@ export const pageQuery = graphql`
 
 const CalendarPage = ({ data }) => {
   const { nodes } = data.allAirtable;
-  const renderedCalendar = nodes.map(node => CalendarEvent(node.data))
+  const renderedCalendar = nodes.map(node => CalendarEventCard(node.data))
   return (
     <Layout fluid={null} fullheight={false}>
       <div className="container"> 
