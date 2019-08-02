9474c9fa4149173153d385081e8d973c172096ae
jacobsilver2 <jacobsilver2@mac.com>
2019-08-02 17:01:46 -0400

    changed random images to png format.  Added a custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const FourOhFour = (props) => (
+  <>
+    <Layout fluid={null} fullheight={false}>
+      <h2>Sorry, we don't know what went wrong.  Let's assume it's your fault.</h2>
+    </Layout>
+  </>
+)
+
+
+export default FourOhFour;
