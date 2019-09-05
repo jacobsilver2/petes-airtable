6de814f397d9b90d649633c653b3634836f61389
jacobsilver2 <jacobsilver2@mac.com>
2019-09-05 18:46:22 -0400

    further work on ReportForm.js

--- src/components/Staff/BarReport.js
+++ src/components/Staff/BarReport.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReportForm from './ReportForm';
+import { StaticQuery, graphql } from "gatsby"
+
+//? As of now, the plan is to make a static query and receive the current days blank report.  
+//? This requires making a new row in Airtable every morning via integromat
+//? In the morning build, the days blank report will be received.
+//? This component will pass down the props 'date, id' and any other fields which might already have data
 
 const BarReport = () => {
   return <p>Future home of the bar report.</p>
--- src/components/Staff/ReportForm.js
+++ src/components/Staff/ReportForm.js
@@ -1,47 +1,32 @@
-import React, {useState} from 'react';
-import Airtable from 'airtable';
-const base = new Airtable({ apiKey: process.env.GATSBY_AIRTABLE_API }).base('app4Eb0X39KtGToOS');
+import React, { useState } from "react"
+import Airtable from "airtable"
+const base = new Airtable({ apiKey: process.env.GATSBY_AIRTABLE_API }).base(
+  "app4Eb0X39KtGToOS"
+)
 
 const ReportForm = ({ date, id, initialNote }) => {
-  const [note, setNote] = useState('')
-  const [bartender, setBartender] = useState('')
-  const [staff, setStaff] = useState()
-  const [reachInBarOpen, setReachInBarOpen] = useState(0)
-  const [reachInBarClosed, setReachInBarClosed] = useState(0)
-  const [redFridgeOpen, setRedFridgeOpen] = useState(0)
-  const [redFridgeClosed, setRedFridgeClosed] = useState(0)
-  const [lowBoyKitchenOpen, setLowBoyKitchenOpen] = useState(0)
-  const [lowBoyKitchenClosed, setLowBoyKitchenClosed] = useState(0)
-  const [walkinOpen, setWalkinOpen] = useState(0)
-  const [walkinClosed, setWalkinClosed] = useState(0)
-  const [buttonName, setButtonName] = useState('Submit')
-  const [buttonStyle, setButtonStyle] = useState('has-background-danger')
+  const [note, setNote] = useState("")
+  const [bartender, setBartender] = useState("")
+  const [buttonName, setButtonName] = useState("Submit")
+  const [buttonStyle, setButtonStyle] = useState("has-background-danger")
 
   function handleSubmit(e) {
-    e.preventDefault();
-    base('Logbook').update(id, {
-      "Notes": note,
-      "Staff": [
-        "rechSv8MDgIA3f7U4"
-      ],
-      "Reach In (bar) Open": reachInBarOpen,
-      "Reach In (bar) Close": reachInBarClosed,
-      "Red Fridge Open": redFridgeOpen,
-      "Red Fridge Close": redFridgeClosed,
-      "Low Boy (kitchen) Open": lowBoyKitchenOpen,
-      "Low Boy (kitchen) Close": lowBoyKitchenClosed,
-      "Walk In Open": walkinOpen,
-      "Walk In Close": walkinClosed
-    }, function(err, record) {
-      if (err) {
-        console.error(err);
-        return;
+    e.preventDefault()
+    base("Logbook").update(
+      id,
+      {
+        Notes: note,
+        Staff: ["rechSv8MDgIA3f7U4"],
+      },
+      function(err, record) {
+        if (err) {
+          console.error(err)
+          return
+        }
+        setButtonName("Successfully Submitted"), setButtonStyle("is-success")
       }
-      console.log(record.get('Date'));
-    });
-    
+    )
   }
-
 
   return (
     <div className="card">
@@ -53,33 +38,52 @@ const ReportForm = ({ date, id, initialNote }) => {
       <div className="card-content">
         <div className="content">
           <div className="field">
-          <form
-            name="bartender report form"
-            onSubmit={e => handleSubmit(e)}
-            method="POST"
-          >
-            <label className="label has-text-white">Bartender Report</label>
-            <div className="field">
-              <p className="control is-expanded">
-                <textarea 
-                  className="textarea"
-                  name="note" 
-                  value={note}
-                  placeholder={initialNote}
-                  onChange={e => setNote(e.target.value)}
-                />
-              </p>
-            </div>
+            <form
+              name="bartender report form"
+              onSubmit={e => handleSubmit(e)}
+              method="POST"
+            >
+              {/* Date is received from props and is not changeable */}
+              <label className="label has-text-white">Bartender Report</label>
+              <div className="field">
+                <p className="control is-expanded">
+                  <textarea
+                    className="textarea"
+                    name="note"
+                    value={note}
+                    placeholder={initialNote}
+                    onChange={e => setNote(e.target.value)}
+                  />
+                </p>
+              </div>
+              <br />
 
-            <br />
+              {/* Still thinking about how to do staff */}
+              <label className="label has-text-white">Staff</label>
+              <div className="field">
+                <p className="control is-expanded">
+                  <textarea
+                    className="text"
+                    name="bartender"
+                    value={bartender}
+                    onChange={e => setBartender(e.target.value)}
+                  />
+                </p>
+              </div>
 
-            
-            
-          </form>
-
+              <button
+                type="submit"
+                className={`button is-link is-fullwidth ${buttonStyle}`}
+                onSubmit={e => handleSubmit(e)}
+              >
+                {buttonName}
+              </button>
+            </form>
           </div>
         </div>
       </div>
     </div>
   )
 }
+
+export default ReportForm
