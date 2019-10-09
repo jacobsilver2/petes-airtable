ad9c9c5da222cd62864dea73ad7778cb08d394ec
Jacob Silver <jacobsilver2@mac.com>
2019-10-09 13:39:20 -0400

    reading series hotfix
    fixed a bug with reading series links.  created a conditional to allow for links which started with http.

--- src/utility/createHtml.js
+++ src/utility/createHtml.js
@@ -29,12 +29,22 @@ export default function createHtml(data) {
           <section className="section">
             <div key={data.id} className="level">
               <div className="level-item">
-                <Link
-                  to={data.website}
-                  className="button is-large is-primary is-outlined is-rounded is-inverted"
-                >
-                  {data.Content}
-                </Link>
+                {data.website.startsWith("http") ? (
+                  <a
+                    target="_blank"
+                    className="button is-large is-primary is-outlined is-rounded is-inverted"
+                    href={data.website}
+                  >
+                    {data.Content}
+                  </a>
+                ) : (
+                  <Link
+                    to={data.website}
+                    className="button is-large is-primary is-outlined is-rounded is-inverted"
+                  >
+                    {data.Content}
+                  </Link>
+                )}
               </div>
             </div>
           </section>
