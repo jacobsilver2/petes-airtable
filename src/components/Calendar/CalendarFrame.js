a2c8c32e06d73ac73d03f08f52536558ffa6430e
Jacob Silver <jacobsilver2@mac.com>
2019-10-11 13:21:05 -0400

    code cleanup
    just some code cleanup in the calendar files

--- src/components/Calendar/CalendarFrame.js
+++ src/components/Calendar/CalendarFrame.js
@@ -8,13 +8,10 @@ import formatCalendarTime from "../../utility/formatCalendarTime"
 import { formatCalendarDate2 } from "../../utility/formatCalendarDate"
 
 const CalendarFrame = ({ events, data: { allFile }, firstEvents }) => {
-  // let prevDate = ""
-  // const renderedEvents = []
   const myEvents = events.map(event => (
     <CalendarEvent
       isFirstEvent={firstEvents.includes(event.id)}
       date={formatCalendarDate(event.fields.Date)}
-      dateTwo={formatCalendarDate2(event.fields.Date)}
       time={formatCalendarTime(event.fields.Date)}
       image={
         event.fields["Act Image"]
@@ -38,37 +35,6 @@ const CalendarFrame = ({ events, data: { allFile }, firstEvents }) => {
       key={event.id}
     />
   ))
-
-  // events.forEach(event => {
-  //   const theEvent = (
-  //     <CalendarEvent
-  //       // isFirstEvent={!moment(event.fields.Date).isSame(prevDate, "day")}
-  //       isFirstEvent={}
-  //       date={formatCalendarDate(event.fields.Date)}
-  //       dateTwo={formatCalendarDate2(event.fields.Date)}
-  //       time={formatCalendarTime(event.fields.Date)}
-  //       image={
-  //         event.fields["Act Image"]
-  //           ? event.fields["Act Image"][0].url
-  //           : getRandomImage(allFile.nodes)
-  //       }
-  //       title={event.fields.Name}
-  //       blurb={event.fields["Act Blurb"] ? event.fields["Act Blurb"][0] : null}
-  //       website={
-  //         event.fields["Act Website"] ? event.fields["Act Website"][0] : null
-  //       }
-  //       soundcloud={
-  //         event.fields["Act Soundcloud"]
-  //           ? event.fields["Act Soundcloud"][0]
-  //           : null
-  //       }
-  //       id={event.id}
-  //       key={event.id}
-  //     />
-  //   )
-  //   renderedEvents.push(theEvent)
-  //   prevDate = event.fields.Date
-  // })
 
   return <Wrapper>{myEvents}</Wrapper>
 }
