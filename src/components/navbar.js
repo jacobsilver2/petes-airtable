b7df9531f713eeca8a46f7178853b1348f321baa
Jacob Silver <jacobsilver2@mac.com>
2019-10-12 04:32:40 -0400

    code cleanup
    removed comments and unused code in the header

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,33 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react"
 import PropTypes from "prop-types"
-// import { Link, navigate } from "gatsby"
-// import { getUser, isLoggedIn, logout } from "../services/auth"
-import filterNavItems from '../utility/filterNavItems';
+import filterNavItems from "../utility/filterNavItems"
 
-
-const Navbar = ({ siteTitle, navItems }) => {
-  // const  [active, setActive]  = useState(false);
-  const [navBarActiveClass, setNavBarActiveClass] = useState('');
-
-  // const content = { message: "", login: true }
-  // if (isLoggedIn()) {
-  //   content.message = `Hello, ${getUser().name}`
-  // } else {
-  //   content.message = "You are not logged in"
-  // }
+const Navbar = ({ navItems }) => {
+  const [navBarActiveClass, setNavBarActiveClass] = useState("")
 
   function toggleHamburger(e) {
-    e.preventDefault();
-    // setActive(!active);
-    // active ? setNavBarActiveClass('is-active') : setNavBarActiveClass('')
-    navBarActiveClass === 'is-active' ? setNavBarActiveClass('') : setNavBarActiveClass('is-active')
+    e.preventDefault()
+    navBarActiveClass === "is-active"
+      ? setNavBarActiveClass("")
+      : setNavBarActiveClass("is-active")
   }
 
   return (
-    <nav className="navbar is-transparent is-fixed-top" role="navigation" aria-label="main-navigation">
-    <div className="container">
+    <nav
+      className="navbar is-transparent is-fixed-top"
+      role="navigation"
+      aria-label="main-navigation"
+    >
+      <div className="container">
         {/* Hamburger Menu */}
-        <div className={`navbar-burger burger ${navBarActiveClass}`} data-target="navMenu" onClick={(e) => toggleHamburger(e)}>
+        <div
+          className={`navbar-burger burger ${navBarActiveClass}`}
+          data-target="navMenu"
+          onClick={e => toggleHamburger(e)}
+        >
           <span />
           <span />
           <span />
@@ -38,13 +35,13 @@ const Navbar = ({ siteTitle, navItems }) => {
           {filterNavItems(navItems)}
         </div>
       </div>
-  </nav>
+    </nav>
   )
 }
 
 Navbar.propTypes = {
   siteTitle: PropTypes.string,
-  navItems: PropTypes.array
+  navItems: PropTypes.array,
 }
 
 Navbar.defaultProps = {
